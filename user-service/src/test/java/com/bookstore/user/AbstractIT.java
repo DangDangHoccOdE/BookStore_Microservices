301788c8c36f6096381cbf8e4a11edd91244c036
DangDangHocCode <haidang972004@gmail.com>
2026-04-28 21:47:10 +0700

    refactor(AbstractIT): improve formatting and consistency in dynamic property registration

--- user-service/src/test/java/com/bookstore/user/AbstractIT.java
+++ user-service/src/test/java/com/bookstore/user/AbstractIT.java
@@ -51,24 +51,24 @@ public abstract class AbstractIT {
     @Autowired
     protected UserProfileRepository userProfileRepository;
 
-        @DynamicPropertySource
-        static void registerDynamicProperties(DynamicPropertyRegistry registry) {
-            String issuerUri = KEYCLOAK.getAuthServerUrl() + "/realms/" + REALM;
+    @DynamicPropertySource
+    static void registerDynamicProperties(DynamicPropertyRegistry registry) {
+        String issuerUri = KEYCLOAK.getAuthServerUrl() + "/realms/" + REALM;
 
-            registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
-            registry.add("spring.datasource.username", POSTGRES::getUsername);
-            registry.add("spring.datasource.password", POSTGRES::getPassword);
+        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRES::getUsername);
+        registry.add("spring.datasource.password", POSTGRES::getPassword);
 
-            registry.add("spring.security.oauth2.resourceserver.jwt.issuer-uri", () -> issuerUri);
-            registry.add("security.jwt.required-audience", () -> REQUIRED_AUDIENCE);
+        registry.add("spring.security.oauth2.resourceserver.jwt.issuer-uri", () -> issuerUri);
+        registry.add("security.jwt.required-audience", () -> REQUIRED_AUDIENCE);
 
-            // Must match your ApplicationProperties(prefix = "users")
-            registry.add("users.keycloak.server-url", KEYCLOAK::getAuthServerUrl);
-            registry.add("users.keycloak.realm", () -> REALM);
-            registry.add("users.keycloak.client-id", () -> CLIENT_ID);
-            registry.add("users.keycloak.client-secret", () -> CLIENT_SECRET);
-            registry.add("users.keycloak.default-role", () -> "USER");
-        }
+        // Must match your ApplicationProperties(prefix = "users")
+        registry.add("users.keycloak.server-url", KEYCLOAK::getAuthServerUrl);
+        registry.add("users.keycloak.realm", () -> REALM);
+        registry.add("users.keycloak.client-id", () -> CLIENT_ID);
+        registry.add("users.keycloak.client-secret", () -> CLIENT_SECRET);
+        registry.add("users.keycloak.default-role", () -> "USER");
+    }
 
     @BeforeEach
     void cleanDatabase() {
